b8aa65030e902789e01c3fd965f7e763551df3c3
jacqueline-s-w <jacqueline.scharrer@web.de>
2026-02-16 17:38:49 +0100

    feat: enhance recipe matching logic to normalize user and recipe ingredients

--- backend/routes/recipes.py
+++ backend/routes/recipes.py
@@ -34,19 +34,25 @@ recipes=[
 
 # def normalize(text):
 #     return re.sub(r"\s+", " ", text.lower().strip())
-def find_matching_recipes(ingredients, recipes):
-    normalized_ingredients=[i.lower().strip() for i in ingredients]
+def find_matching_recipes(user_ingredients, recipes):
+    # User-Zutaten normalisieren
+    normalized_user_ingredients=[ingredient.lower().strip() for ingredient in user_ingredients]
+
     result=[]
+
     for recipe in recipes:
-            recipe_ingredients=[ri.lower() for ri in recipe["ingredients"]]
-            for ingredient in normalized_ingredients:
-                if ingredient in recipe_ingredients:
+            # Rezept-Zutaten normalisieren
+            normalized_recipe_ingredients=[ingredient.lower().strip() for ingredient in recipe["ingredients"]]
+
+            # Prüfen, ob mindestens eine Zuatat passt
+            for ingredient in normalized_user_ingredients:
+                if ingredient in normalized_recipe_ingredients:
                      result.append(recipe)
-                     break #Rezept einmal gefunden -> weiter
-    #             if not result:
-    #                 ai_recipe = generate_recipe_with_ai(ingredients)
-    # result.append(ai_recipe)
-    
+                     break #Rezept nur einmal hinzufügen
+                if not result:
+                     ai_recipe = generate_recipe_with_ai(user_ingredients)
+     result.append(ai_recipe)
+    #_________________________________???
     return result
                 
 
